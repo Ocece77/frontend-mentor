75873818cca54a000e239d9c5263fc416bdde745
Ocece77 <oceanekasindupro@gmail.com>
2023-08-21 02:32:52 +0200

    creating a home page for my project

---
+++ app.js
@@ -0,0 +1,36 @@
+/*List of Project */
+const project= [
+
+  {
+    id : 1,
+    img : "../project1/images/image-qr-code.png",
+    url : "./project1/index.html",
+    title: "QR-CODE",
+   
+  },
+
+
+]
+
+
+const containterList = document.querySelector(".containter-list")
+
+window.addEventListener('DOMContentLoaded',() => {
+  displayProject(project);
+});
+
+const displayProject = (project) =>{
+
+/*map over the array and display an li */
+  let display = project.map(item =>{
+
+    return `<li>
+    <img src=${item.img} alt="qr-code" id=${item.id}>
+    <h4 class="container-list_title">${item.title}</h4>
+    <a href=${item.url} target="_blank" rel="noopener noreferrer">View the project</a>
+  </li>`
+  }).join("")
+
+/* add the li in the inner html */
+  containterList.innerHTML= display
+}
